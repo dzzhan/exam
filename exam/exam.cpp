6e20d2e7806e7084931f25cd28e11f0debb3f23f
dzzhan <dzzhan@163.com>
2019-08-17 11:25:55 +0800

    增加全排列算法

--- exam/exam.cpp
+++ exam/exam.cpp
@@ -39,7 +39,10 @@ void QuickSort(int* s, int left, int right) {
 	QuickSort(s, i + 1, right);
 }
 
-
+//堆排序是将数据看成是完全二叉树、根据完全二叉树的特性来进行排序的一种算法
+//最大堆要求节点的元素都要不小于其孩子，最小堆要求节点元素都不大于其左右孩子
+//那么处于最大堆的根节点的元素一定是这个堆中的最大值
+//构造一个最大堆
 void MaxHeapify(int arr[], int start, int end) {
 	// 建立父節點指標和子節點指標
 	int dad = start;
@@ -69,9 +72,59 @@ void HeapSort(int arr[], int len) {
 	}
 }
 
+//全排列算法
+//比如，假设集合是{ a,b,c }, 那么这个集合中元素的全部排列是{ (a,b,c),(a,c,b),(b,a,c),(b,c,a),(c,a,b),(c,b,a) }，
+//显然，给定n个元素共同拥有n!种不同的排列，假设给定集合是{ a,b,c,d }，能够用以下给出的简单算法产生其全部排列，
+//即集合(a, b, c, d)的全部排列有以下的排列组成：
+//（1）以a开头后面跟着(b, c, d)的排列
+//（2）以b开头后面跟着(a, c, d)的排列
+//（3）以c开头后面跟着(a, b, d)的排列
+//（4）以d开头后面跟着(a, b, c)的排列
+void Permutation(int* a, int k, int m)
+{
+	int i, j;
+	if (k == m)
+	{
+		for (i = 0; i <= m; i++)
+			cout << a[i];
+		cout << endl;
+	}
+	else
+	{
+		for (j = k; j <= m; j++)
+		{
+			Swap(&a[j], &a[k]);
+			Permutation(a, k + 1, m);
+			swap(a[j], a[k]);
+		}
+	}
+}
+
+//求幂次
+double qpow(double a, long long b) {
+	double res = 1;
+	while (b) {
+		if (b & 1) res = res * a;
+		b >>= 1;
+		a *= a;
+	}
+	return res;
+}
+
+
+double myPow(double x, long long n) {
+	if (n == 0) return 1;
+	if (n > 0) return qpow(x, n);
+	if (n < 0) return 1 / qpow(x, -n);
+	return 1.0;
+}
+
 int main()
 {
 	int s[] = { 1, 2, 4, 3, 8, 9 };
 	//QuickSort(s, 0, (sizeof(s) / sizeof(int)) - 1);
 	HeapSort(s, (sizeof(s) / sizeof(int)));
+
+	int a[] = { 1, 2, 3, 4, 5 };
+	Permutation(a, 0, 4);
 }
